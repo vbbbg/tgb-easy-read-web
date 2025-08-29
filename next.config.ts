8d6789a5dbfc3fc0f4542f0eb5cbb590374bc66d
idwang <18909076549@qq.com>
2025-08-29 17:02:46 +0800

    page-init

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'css.tgb.cn',
+        port: '',
+        pathname: '/images/face/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co', // For avatar fallbacks
+      },
+    ],
+  },
 }
 
 export default nextConfig
